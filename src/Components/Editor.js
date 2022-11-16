68eb6cf254500c13b7a9eb58c83c2d92e687a75d
marwa <marwajawad19@gmail.com>
2022-11-16 20:50:19 +0300

    creat Editor component

---
+++ src/Components/Editor.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+function Editor(){
+
+return(
+
+<div className="bottom-pane">
+<iframe
+srcDoc=""
+/*Title option*/
+/*sandbox option*/
+/*frameBorder option*/
+/*width-height option*/
+/*Title option*/
+></iframe>
+</div>
+
+
+)
+
+
+
+}
+export default Editor;
